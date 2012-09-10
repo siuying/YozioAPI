00e59174d43a4665db1b828b644a279eb6dc4a44
Francis Chong <francis@ignition.hk>
2012-09-10 11:58:31 +0800

    Initial Commit

---
+++ YozioAPI/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  YozioAPI
+//
+//  Created by Francis Chong on 12年9月10日.
+//  Copyright (c) 2012年 Ignition Soft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
